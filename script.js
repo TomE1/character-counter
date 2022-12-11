6ad9daec2850d5356bf4f882cdba01e2e56dee6d
TomE <tommevans210@gmail.com>
2022-12-11 07:33:42 +0700

    added comments in js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// count is on the letter 0
 const count = document.querySelector('.count');
+// input is the text field. There's a placeholder on it.
 const input = document.querySelector('input');
 
+// keyup indicates when a key is released from the keyboard
 input.addEventListener('keyup', () => {
   count.innerHTML = input.value.length;
 });
